857a9346b47330b5bab27a3b734e5068eb187ec9
Elina <elinakurtumerova23.07@gmail.com>
2025-05-27 21:23:40 +0300

    oops

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def qr():
    
    buffer = BytesIO()
    img.save(buffer, format="png")
- 
+
+
    img64 = base64.b64encode(buffer.getvalue())
    return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
 
